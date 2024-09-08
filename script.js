d2e50f94a0eba38b0090e6393f9195fe443a136e
Anukarsh <anukarshr@gmail.com>
2024-09-08 20:01:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ async function main(){
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text()
     console.log(response)
+    let element = document.createElement("div")
 }
 
 main()
